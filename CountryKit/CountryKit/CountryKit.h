59750e3bbcfdb53c98a2ae16f3b9eee6b418dd09
Romain Rivollier <romain.rivollier@gmail.com>
2016-04-10 18:42:09 +0200

    initial commit

---
+++ CountryKit/CountryKit/CountryKit.h
@@ -0,0 +1,19 @@
+//
+//  CountryKit.h
+//  CountryKit
+//
+//  Created by Romain on 10/04/16.
+//  Copyright © 2016 Romain Rivollier. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CountryKit.
+FOUNDATION_EXPORT double CountryKitVersionNumber;
+
+//! Project version string for CountryKit.
+FOUNDATION_EXPORT const unsigned char CountryKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CountryKit/PublicHeader.h>
+
+
